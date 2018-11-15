8bc94af98b6e511158915471fd2dc614303bc241
Viktor Kirillov <mr.kirillov@icloud.com>
2018-11-14 23:12:01 -0600

    Added Google Maps support & rendering in result

---
+++ src/components/google_map.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class GoogleMap extends Component {
+
+  // Runs exactly after render
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 11,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+
+  render() {
+    return <div ref="map" />;
+  }
+}
+
+export default GoogleMap;
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,16 +2,20 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
 
+import GoogleMap from '../components/google_map';
+
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressure = cityData.list.map(weather => weather.main.pressure);
     const humidity = cityData.list.map(weather => weather.main.humidity); 
+    const {lon, lat} = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td><strong>{name}</strong></td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        {/* <td><strong>{name}</strong></td> */}
         <td><Chart data={temps} color="orange" units="K" /></td>
         <td><Chart data={pressure} color="green" units="hPa" /></td>
         <td><Chart data={humidity} color="blue" units="%" /></td>
